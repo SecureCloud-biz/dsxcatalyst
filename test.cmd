e0e9086f3d5f8e035a5bb28c9475faf674bb465e
Dave Parsons <dave@daveparsons.net>
2016-10-20 12:43:59 +0100

    New Windows test script.

---
+++ test.cmd
@@ -0,0 +1,16 @@
+@echo on
+curl -X GET localhost:8080/api/vms
+curl -d '{"id":"dave1", "name":"donk", "tag":"janus"}' -X POST localhost:8080/api/vms
+curl -d '{"id":"dave2", "parentid":"dave1", "name":"yeti", "tag":"edna"}' -X POST localhost:8080/api/vms
+curl -d 'on' -X PATCH localhost:8080/api/vms/power/dave1
+curl -d 'on' -X PATCH localhost:8080/api/vms/power/dave1
+curl -d 'off' -X PATCH localhost:8080/api/vms/power/dave1
+curl -d 'on' -X PATCH localhost:8080/api/vms/power/dave1
+curl -d 'shutdown' -X PATCH localhost:8080/api/vms/power/dave1
+curl -d 'on' -X PATCH localhost:8080/api/vms/power/dave1
+curl -d 'suspend' -X PATCH localhost:8080/api/vms/power/dave1
+curl -d 'on' -X PATCH localhost:8080/api/vms/power/dave1
+curl -d 'pause' -X PATCH localhost:8080/api/vms/power/dave1
+curl -d 'unpause' -X PATCH localhost:8080/api/vms/power/dave1
+curl -X GET localhost:8080/api/vms/dave1/ipaddress
+curl -d 'shutdown' -X PATCH localhost:8080/api/vms/power/dave1
