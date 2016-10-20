4ab7eac29850f08df15e8af86aff84e85371bb33
David Parsons <dave@daveparsons.net>
2016-10-20 13:34:32 +0100

    Additional tests for delete VM.

--- test.sh
+++ test.sh
@@ -14,3 +14,5 @@ curl -d 'pause' -X PATCH localhost:8080/api/vms/power/dave1
 curl -d 'unpause' -X PATCH localhost:8080/api/vms/power/dave1
 curl -X GET localhost:8080/api/vms/dave1/ipaddress
 curl -d 'shutdown' -X PATCH localhost:8080/api/vms/power/dave1
+curl -X DELETE localhost:8080/api/vms/dave1
+curl -X DELETE localhost:8080/api/vms/dave2
